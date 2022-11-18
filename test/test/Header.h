088d9404c05993760f073fc677ef68c55265c2ed
Joe Hicks <103856984+CEOofBeans@users.noreply.github.com>
2022-11-18 12:59:25 -0500

    Add files via upload

---
+++ test/test/Header.h
@@ -0,0 +1,35 @@
+#pragma once
+#pragma once
+#include <iostream>
+#ifndef QUEUE_H
+#define QUEUE_H
+
+struct QNode
+{
+	string data;
+	QNode* next;
+};
+
+typedef QNode* QNodePtr;
+
+class Queue
+{
+private:
+	QNodePtr front; // Points to head. Items are removed
+	QNodePtr back; // Points to end node. Items are removed
+	QNodePtr temp;
+public:
+	Queue()
+	{
+		front = NULL;
+		back = NULL;
+		temp = NULL;
+	}
+
+	Queue(const Queue& aQueue);
+	~Queue() {	}
+	void insert(string& val);
+	char remove();
+	void display();
+};
+#endif // QUEUE_H
